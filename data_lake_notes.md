cff20026e8dbe4554f62146b8e6b7f035600c6e0
jyablonski9 <jyablonski9@gmail.com>
2023-03-16 16:51:42 -0500

    add new iceberg notes

--- data_lake_notes.md
+++ data_lake_notes.md
@@ -4,3 +4,46 @@
 [Article](https://alexmercedcoder.medium.com/configuring-apache-spark-for-apache-iceberg-74ad039cdb6e)
 [Article 2](https://github.com/apache/iceberg/issues/3546)
 [Iceberg Official Notes](https://iceberg.apache.org/docs/latest/aws/)
+
+
+DynamoDB can be used to perform table locking in S3 to enable ACID Transactions.
+
+AWS Glue Catalog can be used to store table information in Glue Tables under a Glue Database.  
+- Anytime a schema change occurs, A Glue TableVersion is created. 
+- Tables have a Version ID attached to them in their metadata.  When they're updated, this ID is incrementally increased.  This is used as a table locking feature.  
+- Users can update the table as long as this ID has not been changed on the server side.  If it's different, the update fails and you'll have to refresh the metadata to retrieve the most up-to-date version.
+
+Warehouse is a term used to refer to the root location in S3.  Looks like `s3://jyablonski-iceberg/my_db/` and a table in it would look like `s3://jyablonski-iceberg/my_db/table1`.
+
+Create Iceberg tables using the `USING iceberg` syntax in a `CREATE TABLE` statement.
+```
+CREATE TABLE my_catalog.my_ns.my_table (
+    id bigint,
+    data string,
+    category string)
+USING iceberg
+OPTIONS ('location'='s3://my-special-table-bucket')
+PARTITIONED BY (category);
+```
+
+Time Travel features are enabled via turning on File Versioning in the Data Lake.
+
+# Spark Config
+
+There can only be 1 `spark.jars.packages` in a Spark Config.  If there are more they will just overwrite each other.
+
+```
+from pyspark.sql import SparkSession
+
+spark_packages = [
+    'org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.1.0',
+    'software.amazon.awssdk:bundle:2.16.43',
+    'software.amazon.awssdk:url-connection-client:2.16.43',
+    'org.apache.hadoop:hadoop-aws:3.3.1',
+]
+
+
+spark = SparkSession \
+    .builder \
+    .config('spark.jars.packages', ','.join(spark_packages)) \
+```
---
+++ iceberg/final.py
@@ -0,0 +1,62 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+import time
+import os
+
+# https://github.com/apache/iceberg/issues/3829
+spark_packages = [
+    'org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.1.0',
+    # 'software.amazon.awssdk:bundle:2.16.43',
+    # 'software.amazon.awssdk:url-connection-client:2.16.43',
+    'org.apache.hadoop:hadoop-aws:3.3.1',
+]
+
+catalog_name = 'iceberg_dev'
+
+# this is used for ACID transactions to lock the table from being changed while an insert update or delete is in progress.
+# .config(f'spark.sql.catalog.{catalog_name}.lock-impl', 'org.apache.iceberg.aws.glue.DynamoLockManager') \
+# .config(f'spark.sql.catalog.{catalog_name}.lock.table', f'{catalog_name}') \
+
+spark = SparkSession \
+    .builder \
+    .config('spark.jars.packages', ','.join(spark_packages)) \
+    .config('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions') \
+    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+    .config('spark.hadoop.fs.s3a.access.key', os.environ.get('aws_access_key_id')) \
+    .config('spark.hadoop.fs.s3a.secret.key', os.environ.get('aws_secret_access_key')) \
+    .config(f'spark.sql.catalog.{catalog_name}', 'org.apache.iceberg.spark.SparkCatalog') \
+    .config(f'spark.sql.catalog.{catalog_name}.warehouse', f's3://jyablonski-iceberg/{catalog_name}') \
+    .config(f'spark.sql.catalog.{catalog_name}.catalog-impl', 'org.apache.iceberg.aws.glue.GlueCatalog') \
+    .config(f'spark.sql.catalog.{catalog_name}.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO') \
+    .getOrCreate()
+
+subName = 'product.testtable'
+tableName = f"{catalog_name}.{subName}"
+spark.sql(f"DROP TABLE IF EXISTS {tableName}")
+spark.sql(f"CREATE TABLE {tableName} (id bigint NOT NULL, data string) USING iceberg")
+spark.sql(f"INSERT INTO TABLE {tableName} VALUES (1, 'a')")
+time.sleep(3)
+spark.sql(f"INSERT INTO TABLE {tableName} VALUES (2, 'b')")
+expiretime = str(spark.sql(f"SELECT * FROM {tableName}.snapshots").tail(1)[0]['committed_at'])
+spark.sql(f"CALL {catalog_name}.system.expire_snapshots('{subName}', TIMESTAMP '{expiretime}')").show()
+
+
+data = spark.createDataFrame([
+ ("100", "2015-01-01", "2015-01-01T13:51:39.340396Z"),
+ ("101", "2015-01-01", "2015-01-01T12:14:58.597216Z"),
+ ("102", "2015-01-01", "2015-01-01T13:51:40.417052Z"),
+ ("103", "2015-01-01", "2015-01-01T13:51:40.519832Z")
+],["id", "creation_date", "last_update_time"])
+
+# STEP 3 Write it back to S3
+# df5.write.mode("overwrite").format("csv").save(s3_dest_path)
+data.coalesce(1).write.parquet('s3a://jacobsbucket97-dev/pyspark/test.parquet')
+
+df = spark.read.parquet('s3a://jyablonski-iceberg/reddit_comment_data-2023-03-06.parquet', inferSchema = True, header = True)
+
+## Write a DataFrame as a Iceberg dataset to the Amazon S3 location.
+spark.sql("""CREATE TABLE IF NOT EXISTS spark_catalog.iceberg_test.iceberg_table (id string,
+creation_date string,
+last_update_time string)
+USING iceberg""")
+          
