654f97a3e2a85f379d3db4231ca9d39159469746
jyablonski9 <jyablonski9@gmail.com>
2023-09-26 19:39:41 -0500

    add that yo

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,28 @@
 from pyspark.sql import SparkSession
 
-def setup_spark_app(app_name: str, spark_packages=None | list[str], local=True):
+
+def setup_spark_app(
+    app_name: str,
+    spark_packages: list[str] | None = None,
+    spark_host: str | None = None,
+):
     """
     Set up a PySpark application.
 
     Parameters:
         app_name (str): Name of the Spark application.
 
-        spark_packages (list): List of Spark packages to include.
+        spark_packages (None | str): Optional list of Spark packages to include.
 
-        local (bool): Whether to connect to a local Spark cluster or not.
+        spark_host (None | str): Optional Parameter to specify the Spark Host if
+            connecting to a Remote Cluster.  If not specified then a local Spark
+            Session is built.
 
     Returns:
-        SparkSession: PySpark SparkSession object.
+        SparkSession: PySpark SparkSession object
+
+    Example:
+        >>> spark = setup_spark_app(f"test")
     """
 
     # Configure Spark
@@ -21,13 +31,15 @@ def setup_spark_app(app_name: str, spark_packages=None | list[str], local=True):
     if spark_packages:
         spark_builder.config("spark.jars.packages", ",".join(spark_packages))
 
-    if local:
+    if spark_host:
+        print(f"Building SparkSession to Remote Spark Cluster at spark://{spark_host}")
+
+        # spark://<master_url>:<master_port>
+        spark_builder.master(f"spark://{spark_host}>")
+    else:
+        print(f"Building SparkSession on Local Machine")
         # Set up for local mode
         spark_builder.master("local[*]")
-    else:
-        # Set up for connecting to a remote Spark cluster
-        # Update the master URL accordingly for your remote Spark cluster
-        spark_builder.master("spark://<master_url>:<master_port>")
 
     spark = spark_builder.getOrCreate()
     return spark
